e690951ce7d957abb2007286bdfdcade257f1adf
Leonelsk8 <leonelgomez.sk8@gmail>
2023-05-03 23:03:59 -0300

    solucionando

--- services/users.services.js
+++ services/users.services.js
@@ -1,4 +1,4 @@
-const User = require('../models/users.model');
+const User = require('../models/users.model.js');
 
 const createNewUser = async (userData) => {
   const newUser = new User(userData);
