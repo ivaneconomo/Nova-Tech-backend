5b5a9f55ab1f7b174c27bec64bedf48451020c8e
ivaneconomo <104017304+ivaneconomo@users.noreply.github.com>
2023-05-08 11:42:07 -0300

    añadiendo rutas de usuario

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,11 @@
-const { createNewUser } = require('../services/users.services');
+const User = require('../models/users.model');
+const {
+  saveUser,
+  findUsers,
+  findUser,
+  updateUser,
+  deleteUserById,
+} = require('../services/users.services');
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
 
@@ -13,13 +20,107 @@ const createUser = async (req, res) => {
     const salt = bcrypt.genSaltSync(10);
     userData.password = bcrypt.hashSync(userData.password, salt);
 
-    await createNewUser(userData);
+    await saveUser(userData);
     res.status(200).json('Se ha registrado con éxito.');
   } catch (error) {
     res.status(500).json(error.message);
   }
 };
 
+const editUser = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { id } = req.params;
+    const userNewData = req.body;
+    const userData = await User.findById(id);
+    const passwordIsValid = await bcrypt.compare(
+      userNewData.password,
+      userData.password
+    );
+    if (!passwordIsValid) {
+      return res.status(401).json({ message: 'Contraseña incorrecta' });
+    }
+    if (userNewData.newPassword) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(userNewData.newPassword, salt);
+      userData.password = hashedPassword;
+    }
+
+    const editedUser = await updateUser(id, userData);
+    if (!editedUser) {
+      res.status(404).json('Usuario no encontrado.');
+      return;
+    }
+    res.status(200).json(editedUser);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await findUsers();
+    if (users.length === 0) {
+      res.status(200).json('No existen usuarios registrados.');
+      return;
+    }
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await findUser(id);
+    if (!user) {
+      res.status(404).json('Usuario no encontrado.');
+      return;
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await deleteUserById(id);
+    if (!deletedUser) {
+      res.status(404).json('Usuario no encontrado.');
+      return;
+    }
+    res.status(200).json(deletedUser);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+const disableUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const disabled = { disabled: true };
+    const disabledUser = await updateUser(id, disabled);
+    if (!disabledUser) {
+      res.status(404).json('Usuario no encontrado.');
+      return;
+    }
+    res.status(200).json(`El usuario con el id: "${id}" ha sido deshabilitado`);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
 module.exports = {
+  getAllUsers,
+  getUserById,
   createUser,
+  editUser,
+  deleteUser,
+  disableUser,
 };
